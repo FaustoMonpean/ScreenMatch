61ef36ea3c4f6767399c4ce19d18878022bf1974
FaustoMonpean <dev.monpean@gmail.com>
2025-09-04 11:51:11 -0300

    Classe Filme, metodo para exibir filme e metodo para avaliar filme criados

--- src/Filme.java
+++ src/Filme.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDeAvaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Filme: "+nome);
+        System.out.println("Ano: "+anoDeLancamento);
+        System.out.println("Duração: "+duracaoEmMinutos+" minutos");
+        System.out.println("Nota: "+ somaDeAvaliacao/totalDeAvaliacoes);
+        if (incluidoNoPlano){
+            System.out.println("Status: Disponível no seu plano!");
+        } else {
+            System.out.println("Status: Indisponível no plano atual.");
+        }
+    }
+    void avalia(double nota){
+        somaDeAvaliacao += nota;
+        totalDeAvaliacoes += 1;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Filme topGun = new Filme();
+        topGun.nome = "Top Gun";
+        topGun.anoDeLancamento = 2022;
+        topGun.incluidoNoPlano = true;
+        topGun.duracaoEmMinutos = 180;
+
+        topGun.avalia(10);
+        topGun.avalia(4);
+        topGun.exibeFichaTecnica();
+    }
 }
